26336e8d238a075f6df54dc6df497baa7c2f1c46
muhammadhamidrazasidtechno <hamidsidtechno@gmail.com>
2025-02-10 16:14:35 +0500

    Update StatisticList.jsx

--- src/DashboardScreen/Statistic/StatisticList.jsx
+++ src/DashboardScreen/Statistic/StatisticList.jsx
@@ -62,7 +62,7 @@ const SubMainDashboard = () => {
       {/* Server Grid */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {dummyStats.servers.map((server, i) => (
-          <ServerCard key={i} index={i} server={server} />
+          <ServerCard key={i} index={i} isStatic={true} />
         ))}
       </div>
     </div>
